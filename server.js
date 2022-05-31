36719eae35319e13ff1546f8b05a2e474a3b7a7f
enajera1 <eriknajera013@gmail.com>
2022-05-31 10:49:17 -0500

    server added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express(); 
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+// Default response for any other request (Not Fount)
+// must be last route listed. It is a catchall
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+}); 
