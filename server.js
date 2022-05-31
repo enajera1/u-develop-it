a7aeda30a87ea791e7b19c69929bb4e182822836
enajera1 <eriknajera013@gmail.com>
2022-05-31 11:17:36 -0500

    server connected to mysql

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express(); 
@@ -7,9 +7,22 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+//Connect to database 
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your SQL username,
+    user: 'root',
+    //Your SQL password
+    password: 'ENZOboltonNajera1223.',
+    database: 'election'
+  },
+  console.log(`Connected to the election database.`)
+);
 
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 // Default response for any other request (Not Fount)
 // must be last route listed. It is a catchall
 app.use((req, res) => {
